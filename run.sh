a53cb776b8f5bc8eab6532287a53557d2cf97b6c
qube <syed.qutub@intel.com>
2025-04-08 21:04:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ export FEATURE_DIR="/nwstore/blueglass/blue_lens_extract"
 
 echo "Found micromamba on the system."
 eval "$(micromamba shell.hook bash)"
-micromamba activate blueglass
+micromamba activate blueglass_env
 
 echo "Using PYTHON : $(which python)"
 nvidia-smi
