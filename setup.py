27f13264582eff24039eef8d9eba82a99d3e639d
Qutub, Syed <syed.qutub@intel.com>
2025-04-11 14:56:03 -0700

    fixed setup file

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # Copyright 2025 Intel Corporation
 # SPDX: Apache-2.0
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="blueglass",
+    packages=find_packages(include=["blueglass", "blueglass.*"]),
+)
